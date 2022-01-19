3595330885ad2f96a58eb03a5351a9b6aeb0b524
lanngoc10a <nguyenngoclan2612ftu@gmail.com>
2022-01-19 01:17:41 +0100

    add

---
+++ Presentation/Class/wizard.cpp
@@ -0,0 +1,19 @@
+//wizard.cpp
+#include <iostream>
+using namespace std;
+#include "wizard.h"
+
+void Wizard::Fight(){
+    cout << "Fighting!" << endl;
+}
+void Wizard::Talk(){
+    cout << "Talking." << endl;
+}
+void Wizard::CastSpell(){
+    cout << "Casting spell." << endl;
+}
+void Wizard::PrintStats(){
+    cout << "Wizard stats:"
+         << "\nName:\t" << m_name 
+         << "\n# hitpoints:\t" << m_hitPoints 
+};
---
+++ build/CMakeFiles/Class.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Class.dir/main.cpp.o"
+  "CMakeFiles/Class.dir/main.cpp.o.d"
+  "Class"
+  "Class.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Class.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

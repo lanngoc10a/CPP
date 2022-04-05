ab70851d5d25ac555d17fa5d4cf916cb0637c63d
Richard <richlones@hotmail.com>
2022-04-05 14:19:13 +0200

    Added startup project

---
+++ assignment3/oblig3/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace oblig3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            dat154Entities dx = new dat154Entities();
+
+            DbSet<student> students = dx.student;
+            
+            foreach (student s in students)
+            {
+                Trace.WriteLine(s.studentname);
+            }
+        }
+    }
+}

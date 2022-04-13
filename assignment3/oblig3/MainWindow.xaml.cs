ce1b2f675e03719929807a34838e558bc70915bf
Richard <richlones@hotmail.com>
2022-04-13 13:35:38 +0200

    Added listview implementation

--- assignment3/oblig3/MainWindow.xaml.cs
+++ assignment3/oblig3/MainWindow.xaml.cs
@@ -41,79 +41,68 @@ namespace oblig3
 
             grades = dx.grade.ToList();
 
+            
             InitializeComponent();
 
             foreach (course c in courses)
             {
                 cmbCourses.Items.Add(c.coursename);
-                
             }
         }
 
-        private void showGrades(List<String> stringGrade)
-        {
-
-            foreach (grade g in grades)
-            {
-                if (stringGrade.Contains(g.grade1))
-                {
-                    outputBox.Text += $"Student: {g.student.studentname}  Karakter: {g.grade1}  Fag: {g.coursecode} \n";
-                }
-            }
-
-            if (outputBox.Text == "")
-            {
-                outputBox.Text = "Ingen resultat";
-            }
-        }
-
-        private void showStudent(String name)
-        {
-            foreach (student s in students)
-            {
-                if (s.studentname == name || s.studentname.ToLower() == name)
-                {
-                    outputBox.Text += $"Student: {s.studentname} \n";
-                }
-            }
-
-            if (outputBox.Text == "")
-            {
-                outputBox.Text = $"Ingen resultat på '{name}'";
-            }
-        }
+        /*
+         *  Task 1
+         *  
+         *  Search for student and show info
+         *  
+         *  Listview contains=[studentid, studentname, studentage]
+         */
       
-
         private void studentButton_Click(object sender, RoutedEventArgs e)
         {
-            outputBox.Text = "";
-            showStudent(sokTekst.Text);
-        }
+            listView.View = null;
+            String name = sokTekst.Text;
 
-        private void karakterButton_click(object sender, RoutedEventArgs e)
-        {
-            outputBox.Text = "";
-            getGrades(sokTekst.Text);
-            
-        }
+            List<grade> studs = new List<grade>();
 
-        private void strykButton_click(object sender, RoutedEventArgs e)
-        {
-            outputBox.Text = "";
-            foreach (grade g in grades)
+            foreach (grade s in grades)
             {
-                if (g.grade1 == "F")
+                if (s.student.studentname == name || s.student.studentname.ToLower() == name)
                 {
-                    outputBox.Text += $"Student: {g.student.studentname} strøk i {g.course.coursename} \n";
+                    s.grade1 = null;
+                    studs.Add(s);
                 }
             }
+
+            listView.ItemsSource = studs;
+
+            if (!studs.Any())
+            {
+                listView.View = null;
+                MessageBox.Show($"Ingen resultat på '{name}', prøv igjen");
+            }
+            else
+            {
+                listView.View = selectListView(false, false);
+
+                listView.ItemsSource = studs;
+            }
         }
+
+        /*
+         *  Task 2
+         *  
+         *  Select course from dropdown list (dropdown list is initialized in MainWindow()
+         *  
+         *  Listview = [studentid, studentname, studentage, grade, coursecode, coursename]
+         */
 
         private void cmbCourses_SelectionChanged(object sender, EventArgs e)
         {
-            outputBox.Text = "";
+            List<grade> studs = new List<grade>();
+
             ComboBox cmb = sender as ComboBox;
-            
+
             foreach (course c in courses)
             {
                 if (c.coursename == cmb.SelectedItem.ToString())
@@ -122,14 +111,86 @@ namespace oblig3
                     {
                         if (s.coursecode == c.coursecode)
                         {
-                            outputBox.Text += s.student.studentname + "   " + s.grade1 + "\n";
+                            studs.Add(s);
                         }
                     }
                 }
             }
+
+            listView.View = selectListView(true, false);
+
+            listView.ItemsSource = studs;
         }
 
-        private void getGrades(String grade)
+        /*
+         *  Task 3
+         *  
+         *  Select grade and show all the grades equal or better, with student information
+         *  
+         *  Listview = [studentid, studentname, studentage, grade, coursecode, coursename]
+         */
+
+        private void karakterButton_click(object sender, RoutedEventArgs e)
+        {
+            listView.View = null;
+
+            List<String> stringGrade = getGrades(sokTekst.Text);
+
+            List<grade> grads = new List<grade>();
+
+            foreach (grade g in grades)
+            {
+                if (stringGrade.Contains(g.grade1))
+                {
+                    grads.Add(g);
+                }
+            }
+
+            if (!grads.Any())
+            {
+                MessageBox.Show($"Ingen resultat på '{sokTekst.Text}', prøv igjen");
+
+            }
+            else
+            {
+                listView.View = selectListView(false, true);
+
+                listView.ItemsSource = grads;
+            }
+        }
+
+        /*
+         *  Task 4
+         *  
+         *  Show all students who failed
+         *  
+         *  Listview = [studentid, studentname, studentage, grade, coursecode, coursename]
+         */
+
+        private void strykButton_click(object sender, RoutedEventArgs e)
+        {
+            List<grade> studs = new List<grade>();
+
+            foreach (grade g in grades)
+            {
+                if (g.grade1 == "F")
+                {
+                    studs.Add(g);
+                }
+            }
+
+            listView.View = selectListView(false, true);
+
+            listView.ItemsSource = studs;
+            
+        }
+
+
+        /*
+         *  Returns grade equal or better to given grade
+         */
+
+        private List<String> getGrades(String grade)
         {
             List<String> grades = new List<String>();
 
@@ -155,8 +216,66 @@ namespace oblig3
                     break;
             }
 
-           showGrades(grades);
+            return grades;
 
+        }
+
+        /*
+         *  Creates listview given what information is wanted
+         */
+
+        private GridView selectListView(bool showCourse, bool showGrade)
+        {
+            GridView myGridView = new GridView();
+            myGridView.AllowsColumnReorder = true;
+
+            GridViewColumn gvc1 = new GridViewColumn();
+            gvc1.DisplayMemberBinding = new Binding("studentid");
+            gvc1.Header = "ID";
+            gvc1.Width = 50;
+            myGridView.Columns.Add(gvc1);
+            GridViewColumn gvc2 = new GridViewColumn();
+            gvc2.DisplayMemberBinding = new Binding("student.studentname");
+            gvc2.Header = "Name";
+            gvc2.Width = 150;
+            myGridView.Columns.Add(gvc2);
+            GridViewColumn gvc3 = new GridViewColumn();
+            gvc3.DisplayMemberBinding = new Binding("student.studentage");
+            gvc3.Header = "Age";
+            gvc3.Width = 50;
+            myGridView.Columns.Add(gvc3);
+
+            if (showCourse)
+            {
+                GridViewColumn gvc4 = new GridViewColumn();
+                gvc4.DisplayMemberBinding = new Binding("grade1");
+                gvc4.Header = "Grade";
+                gvc4.Width = 50;
+                myGridView.Columns.Add(gvc4);
+            }
+
+            if (showGrade)
+            {
+                GridViewColumn gvc4 = new GridViewColumn();
+                gvc4.DisplayMemberBinding = new Binding("grade1");
+                gvc4.Header = "Grade";
+                gvc4.Width = 50;
+                myGridView.Columns.Add(gvc4);
+
+                GridViewColumn gvc5 = new GridViewColumn();
+                gvc5.DisplayMemberBinding = new Binding("course.coursecode");
+                gvc5.Header = "Course code";
+                gvc5.Width = 100;
+                myGridView.Columns.Add(gvc5);
+
+                GridViewColumn gvc6 = new GridViewColumn();
+                gvc6.DisplayMemberBinding = new Binding("course.coursename");
+                gvc6.Header = "Course name";
+                gvc6.Width = 200;
+                myGridView.Columns.Add(gvc6);
+            }
+
+            return myGridView;
         }
     }
 }

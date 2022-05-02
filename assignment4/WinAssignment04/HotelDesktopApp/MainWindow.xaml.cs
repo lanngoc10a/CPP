5288500e120c736d0d93939425cd2adc83279cb1
Richard <richlones@hotmail.com>
2022-05-02 13:17:07 +0200

    Added hotel desktop app

---
+++ assignment4/WinAssignment04/HotelDesktopApp/MainWindow.xaml.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HotelDesktopApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        masterEntities dx = new masterEntities();
+        DbSet<ReservationTable> resTable;
+        DbSet<HotelRoom> roomTable;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+
+            resTable = dx.ReservationTable;
+            resTable.Load();
+            ResList.DataContext = resTable.Local;
+
+
+            roomTable = dx.HotelRoom;
+            roomTable.Load();
+            RoomList.DataContext = roomTable.Local;
+        }
+
+        void SaveChanges(masterEntities context)
+        {
+            context.SaveChanges();
+        }
+        public void AddRes(masterEntities context, ReservationTable res)
+        {
+            dx.ReservationTable.Local.Add(res);
+        }
+        private ReservationTable FindRes(int resID)
+        {         
+            // if not found, run exception
+            return (ReservationTable)dx.ReservationTable.Local.Where(r => r.ResID.Equals(resID)).First();
+        }
+        private void RemoveRes(masterEntities dx, int resID)
+        {
+            ReservationTable res = FindRes(resID);
+            dx.ReservationTable.Local.Remove(res);
+            Console.WriteLine("Deleted resevation: " + resID);
+        }
+
+        private void ButtonAddRes_Click(object sender, RoutedEventArgs e)
+        {
+            new Reservations(dx).ShowDialog();
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            ResList.DataContext = resTable.Local;
+        }
+
+        private void CheckIn()
+        {
+            int resNumb = int.Parse(CheckInResNumb.Text);
+            int RoomNumb = int.Parse(CheckInRoomNumb.Text);
+
+            ReservationTable res = (ReservationTable)dx.ReservationTable.Where(r => r.ResNumb == resNumb).First();
+            HotelRoom room = (HotelRoom)dx.HotelRoom.Where(hr => hr.roomNumb == RoomNumb).First();
+
+            // Check if(res.HotelRoom isEmpty && room.resTable isEmpty)
+            RemoveRes(dx, res.ResID);
+            RemoveRoom(dx, room.roomID);
+
+            res.RoomID = room.roomNumb;
+            room.resNumb = res.ResNumb;
+            room.isUsed = true;
+
+            AddRes(dx, res);
+            AddRoom(dx, room);
+            CheckInResNumb.Text = "";
+            CheckInRoomNumb.Text = "";
+            // else{
+            // Console.WriteLine("Hotelroom is busy or Reservation already have another room");
+            // }
+            SaveChanges(dx);
+        }
+
+        // Updates the choosen room and Reservation in the local DB
+        // puts in the right roomID on the res and the right resID on the room
+        private void CheckInButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                CheckIn();
+            } catch (Exception ex)
+            {
+               
+                Console.WriteLine("Something wrong with Check in! " + ex.Message );
+            }
+            finally
+            {
+                CheckInResNumb.Text = "";
+                CheckInRoomNumb.Text = "";
+            }
+
+        }
+        //adds a room to local DB 
+        private void AddRoom(masterEntities dx, HotelRoom room)
+        {
+            dx.HotelRoom.Local.Add(room);
+        }
+        //removes a roomm from DB 
+        private void RemoveRoom(masterEntities dx, int roomID)
+        {
+            HotelRoom room = FindRoom(roomID);
+            dx.HotelRoom.Local.Remove(room);
+            Console.WriteLine("Deleted room: " + roomID);
+            SaveChanges(dx);
+        }
+        // finds a room with roomID 
+        private HotelRoom FindRoom(int roomID)
+        {
+            // If not found, run exception
+            return dx.HotelRoom.Local.Where(r => r.roomID.Equals(roomID)).First();
+        }
+        
+        private void CheckOut()
+        {
+            int resNumb = int.Parse(CheckOutResNumb.Text);
+            //if( findRes!=null)
+            ReservationTable res = dx.ReservationTable.Local.Where(r => r.ResNumb.Equals(resNumb)).First();
+            int resID = res.ResID;
+            int roomID = (int)res.RoomID;
+
+            RemoveRes(dx, resID);
+
+
+            HotelRoom room = dx.HotelRoom.Local.Where(hr => hr.resNumb == resNumb).First();
+            RemoveRoom(dx, room.roomID);
+
+            // Cleaningstatus = 2, dirty
+            // Cleaningstatus = 1, currently cleaning
+            // Cleaningstatus = 0, Clean
+            room.cleaningStatus = 2;
+
+            room.isUsed = false;
+            room.resNumb = null;
+            AddRoom(dx, room);
+            CheckOutResNumb.Text = "";
+            SaveChanges(dx);
+        }
+        
+        // updates the local DB with the choosen res and room
+        // updates so that the resID is removed from the room 
+        // and the res is removed from the DB
+        private void CheckOutButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                CheckOut();
+            }catch (Exception ex)
+            {
+                Console.WriteLine("Something wrong with check out! " + ex.Message);
+            }
+            finally
+            {
+                CheckOutResNumb.Text = ""; 
+            }
+       
+        }
+    }
+
+
+    
+}
---
+++ assignment4/WinAssignment04/HotelDesktopApp/Reservations.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HotelDesktopApp
+{
+    /// <summary>
+    /// Interaction logic for Reservations.xaml
+    /// </summary>
+    public partial class Reservations : Window
+    {
+
+        private masterEntities dx = new masterEntities();
+        public Reservations(masterEntities context)
+        {
+            InitializeComponent();
+            dx = context;
+        }
+        private void ReservationAdd_Click(object sender, EventArgs e)
+        {
+            if (ValidateResAdd())
+            {
+                try
+                {
+
+                    AddNewRes(dx);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error making reservation! " + ex);
+                }
+                finally
+                {
+                    FirstName.Text = "";
+                    LastName.Text = "";
+                    RoomSize.Text = "";
+                    BedNumber.Text = "";
+                    dx.SaveChanges();
+                }
+            }
+        }
+        public bool ValidateResAdd()
+        {
+            if (FirstName.Text != "" && LastName.Text != "" && StartDate.Text != "" && EndDate.Text != "" && RoomSize.Text != "" && BedNumber.Text != "")
+            {
+                return true;
+            }
+            Console.WriteLine("Please fill in all required information! ");
+            return false;
+        }
+        public void AddRes(masterEntities context, ReservationTable res)
+        {
+            dx.ReservationTable.Local.Add(res);
+        }
+
+        
+        private void AddNewRes(masterEntities context)
+        {
+            // make a unique resNumb
+
+            List<ReservationTable> resList =  context.ReservationTable.Local.ToList();
+            int max = 0; 
+            // finding max resNumb value
+            foreach (ReservationTable r in resList)
+            {
+                if (r.ResNumb > max)
+                    max =(int) r.ResNumb;
+            }
+
+            int resNumb = max+1;
+            string firstName = FirstName.Text;
+            string lastName = LastName.Text;
+            string dateStart = StartDate.Text;
+            string dateEnd = EndDate.Text;
+            int roomSize = int.Parse(RoomSize.Text);
+            int bedNumber = int.Parse(BedNumber.Text);
+
+            ReservationTable res = new ReservationTable();
+
+            res.ResNumb = resNumb;
+            res.FirstName = firstName;
+            res.LastName = lastName;
+            res.DateStart = dateStart;
+            res.DateEnd = dateEnd;
+            res.RoomSize = roomSize;
+            res.BedNumb = bedNumber;
+            res.RoomID = null;
+
+            AddRes(context, res);
+
+        }
+
+        private void ReservationDelete_Click(object sender, EventArgs e)
+        {
+            
+               
+            if (ValidateResDelete())
+            {
+                try
+                {
+                    int resID = int.Parse(DeleteRes.Text);
+                    RemoveRes(dx, resID);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error deleting reservation! " + ex);
+                }
+                finally
+                {
+                    DeleteRes.Text = "";
+                    dx.SaveChanges();
+                }
+            }
+                
+         
+        }
+        private bool ValidateResDelete()
+        {
+            if (DeleteRes.Text != "")
+            {
+                return true;
+            }
+            Console.WriteLine("Please fill in all required information! ");
+            return false;
+        }
+        private void RemoveRes(masterEntities dx, int resID)
+        {
+            ReservationTable res = FindRes(resID);
+            dx.ReservationTable.Local.Remove(res);
+            Console.WriteLine("Deleted resevation: " + resID);
+        }
+
+
+        private ReservationTable FindRes(int resID)
+        {
+            ReservationTable res;
+            res = (ReservationTable)dx.ReservationTable.Local.Where(r=> r.ResID.Equals(resID)).First();
+
+            return res;
+        }
+    }
+}

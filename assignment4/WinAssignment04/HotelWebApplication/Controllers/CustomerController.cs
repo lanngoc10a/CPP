8665b804ba00efc0e0395a9eaf09d5c597a7f88e
Richard <richlones@hotmail.com>
2022-05-03 15:21:45 +0200

    Added web app

---
+++ assignment4/WinAssignment04/HotelWebApplication/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using DBAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HotelWebApplication.Models;
+
+namespace HotelWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+
+       
+        public IEnumerable<HotelRoom> Get()
+        {
+
+            using (masterEntities dx = new masterEntities())
+            {
+                return dx.HotelRoom.ToList();
+                
+            }
+        }
+    }
+}
---
+++ assignment4/WinAssignment04/HotelWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using DBAccess;
+using HotelWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Web;
+
+
+namespace HotelWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Login(DBAccess.Customer customer)
+        {
+            using (masterEntities dx = new masterEntities())
+            {
+                var customerDetails = dx.Customer.Where(x => x.FirstName == customer.FirstName && x.Pass == customer.Pass).FirstOrDefault();
+                if (customerDetails == null)
+                {
+                    customer.LoginErrorMessage = "Wrong ID or password";
+                    return View("Index", customer);
+                }
+
+                else
+                {
+                    dynamic mymodel = new ExpandoObject();
+                    mymodel.Customer = customer;
+                    mymodel.Rooms = dx.HotelRoom.ToList();
+                    return View("Booking", mymodel);
+                }
+            }
+            
+        }
+        [HttpPost]
+        public IActionResult CreateUser(Customer customer)
+        {
+            using (masterEntities dx = new masterEntities())
+            {
+                if (dx.Customer.Any(x => x.FirstName == customer.FirstName && x.LastName == customer.LastName))
+                {
+                    ViewBag.Message = "Username already exists";
+                    return View("Index", customer);
+                }
+                dx.Customer.Add(customer);
+                dx.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.message = "The user " + customer.FirstName + " " + customer.LastName + " is saved successfully";
+            return View("Index", new Customer());
+        }
+
+
+    }
+}
